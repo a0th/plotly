0b935a27f6848bf252beca0759264b1cf0047146
Nilo <renard.agne@gmail.com>
2022-09-28 23:03:37 -0300

    Add overlay_yoy and continuous_lines

--- plasma/plasma.py
+++ plasma/plasma.py
@@ -2,48 +2,50 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 
+import pandas as pd
+import plotly.express as px
 
-def overload_fig():
-    setattr(go.Figure, "fix_labels", fix_labels)
+
+def fluent():
+    setattr(go.Figure, "fix_marker_text", fix_marker_text)
+    setattr(go.Figure, "continuous_lines", continuous_lines)
 
 
-def fix_labels(
+def fix_marker_text(
     fig: go.Figure,
-    panel_format: str = None,
+    marker_format: str = None,
     tick_format: str = None,
-    panel_format_auto_percent: bool = True,
-    tick_format_auto_percent: bool = True,
     panel_font_size: int = 12,
 ) -> go.Figure:
-    if panel_format is None:
-        panel_format = "{:.1f}"
+    if marker_format is None:
+        marker_format = "{:.1f}"
     if tick_format is None:
         tick_format = "{:.1f}"
-    panel_format = panel_format.replace("{:", "%{text:")
-    tick_format = tick_format[1:-1]
-    if panel_format_auto_percent and _is_between_zero_and_one(fig.data[0].text):
-        panel_format = panel_format[:-2] + "%}"
-    if tick_format_auto_percent and _is_between_zero_and_one(fig.data[0].y):
-        tick_format = tick_format[:-2] + "%}"
+    marker_format = marker_format.replace("{:", "%{text:")
 
     return fig.update_traces(
-        texttemplate=panel_format,
+        texttemplate=marker_format,
         textposition="top center",
         textfont=go.scatter.Textfont(size=panel_font_size),
-    ).update_yaxes(tickformat=".1%")
+    ).update_yaxes(tickformat=tick_format[2:-1])
 
 
 def _is_between_zero_and_one(x: np.ndarray) -> bool:
     return (x >= 0).all() and (x <= 1).all()
 
 
-def plot_line_with_secondary_y(
+def dual_axis_line(
     df,
     left_axis: str | list[str],
     right_axis: str | list[str],
     left_axis_title: str = None,
     right_axis_title: str = None,
 ) -> go.Figure:
+    if isinstance(left_axis, str):
+        left_axis = [left_axis]
+    if isinstance(right_axis, str):
+        right_axis = [right_axis]
+
     assert len(set(left_axis) - set(df.columns)) == 0
     assert len(set(right_axis) - set(df.columns)) == 0
     if left_axis_title is None:
@@ -57,15 +59,14 @@ def plot_line_with_secondary_y(
     for c in right_axis:
         fig = fig.add_trace(go.Scatter(x=df.index, y=df[c], name=c), secondary_y=True)
 
-    fig.update_layout(yaxis2=dict(showgrid=False)).update_yaxes(
-        title_text=left_axis_title,
-        secondary_y=False,
-    ).update_yaxes(
-        title_text=right_axis_title,
-        secondary_y=True,
+    return (
+        fig.update_yaxes(title_text=left_axis_title, secondary_y=False)
+        .update_yaxes(
+            title_text=right_axis_title,
+            secondary_y=True,
+        )
+        .update_layout(yaxis2=dict(showgrid=False, zeroline=False))
     )
-
-    return fig
 
 
 def _secondary_axis_title(axis: str | list[str]) -> str:
@@ -73,3 +74,25 @@ def _secondary_axis_title(axis: str | list[str]) -> str:
         return axis
     else:
         return "/".join(axis)
+
+
+def overlay_yoy(df: pd.DataFrame, y: str) -> go.Figure:
+
+    return (
+        px.line(
+            df.assign(_chart_date=lambda df: df.index.strftime("2020-%m-%d")),
+            x="_chart_date",
+            y=y,
+            color="year",
+        )
+        .update_xaxes(tickformat="%b %d")
+        .update_xaxes(title=df.index.name)
+    )
+
+
+def continuous_lines(fig, colorscale: str = "Blues"):
+    n_traces = len(fig.data)
+    colors = px.colors.sample_colorscale(colorscale, n_traces)
+    for index, color in enumerate(colors):
+        fig.data[index]["line"]["color"] = color
+    return fig
