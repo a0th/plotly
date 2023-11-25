67649fd7a864ca531f0cf6f2c26f085ba326e85c
nilo <renard.agne@gmail.com>
2023-11-25 15:33:37 -0300

    Version bump, update description

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name="plotly-plasma",
-    version="0.1",
+    version="0.2",
     package_dir={"": "src"},
     packages=find_packages(),
-    description="A lightweight wrapper to handle some inconveniences with Plotly",
+    description="A small opinionated enhancement for Plotly",
     author="Nilo Araujo",
     author_email="renard.agne@gmail.com",
     url="https://github.com/a0th/plotly",
